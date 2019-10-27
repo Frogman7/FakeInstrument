d2e2f9c3a58087adc9c9d19be51babd27e0c0f76
Frogman7 <Thermonuclear@sbcglobal.net>
2019-10-27 16:35:18 -0700

    Made TCP port configurable

--- FakeInstrument/Program.cs
+++ FakeInstrument/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Linq;
 
 namespace FakeInstrument
 {
     class Program
     {
+        const ushort DefaultTcpPort = 1987;
+
         static void Main(string[] args)
         {
-            FakeInstrument fakeInstrument = new FakeInstrument(1987);
+            ushort tcpPort = DefaultTcpPort;
+
+            if (args.Length > 0)
+            {
+                var portArg = args.FirstOrDefault(arg => arg.StartsWith("port="));
+
+                if (portArg != null)
+                {
+                    string portAsString = portArg.Substring(portArg.IndexOf('=') + 1);
+
+                    if (!ushort.TryParse(portAsString, out tcpPort))
+                    {
+                        Console.WriteLine($"Could not parse port argument '{portAsString}' into a valid port value");
+                    }
+                }
+            }
+
+            FakeInstrument fakeInstrument = new FakeInstrument(tcpPort);
 
             fakeInstrument.Start();
 
